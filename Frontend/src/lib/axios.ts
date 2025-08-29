abbe617d596cb9420743775071d90cfae20d046a
TanviHanda <tanvisona2012@gmail.com>
2025-08-29 21:49:14 +0530

    update: url fixes

--- Frontend/src/lib/axios.ts
+++ Frontend/src/lib/axios.ts
@@ -1,5 +1,12 @@
-import axios from "axios"
-export const axiosInstance = axios.create({
-    baseURL:import.meta.env.VITE_PUBLIC_API_URL+'/api',
-    withCredentials:true,
-})
+import axios from "axios";
+
+// Get the API URL from environment variables
+const API_URL = import.meta.env.VITE_PUBLIC_API_URL;
+
+// Create axios instance with the API URL
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+export { axiosInstance };
