daab5a267af57822d5d881406a51cb0f641c2759
TanviHanda <tanvisona2012@gmail.com>
2025-08-29 22:10:46 +0530

    update: frontend route fix

--- Frontend/src/lib/axios.ts
+++ Frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Get the API URL from environment variables
-const API_URL = import.meta.env.VITE_PUBLIC_API_URL;
+const API_URL = import.meta.env.VITE_PUBLIC_API_URL+"/api";
 
 // Create axios instance with the API URL
 const axiosInstance = axios.create({
